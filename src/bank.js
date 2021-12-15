52ea8cdd5c8369c8691233b23f0537645ff2277b
David Andersson <david.andersson@zocom.se>
2021-12-15 14:15:32 +0100

    Test cases for withdraw and transfer

--- src/bank.js
+++ src/bank.js
@@ -3,7 +3,53 @@ function deposit(account, amount) {
 	if( !isAccount(account) ) {
 		throw new Error('Invalid account object')
 	}
+	else if( !isPositiveNumber(amount) ) {
+		throw new Error('Amount must be a valid number')
+	}
 	account.balance += amount
+}
+
+
+function withdraw(account, amount) {
+	if( !isAccount(account) ) {
+		throw new Error('Invalid account object')
+	}
+	else if( !isPositiveNumber(amount) ) {
+		throw new Error('Amount must be a valid number')
+	}
+	else if( amount > account.balance ) {
+		throw new Error('Not enough money in account')
+	}
+	account.balance -= amount
+}
+
+
+function transfer(source, target, amount) {
+	if( !isAccount(source) || !isAccount(target) ) {
+		return false
+	}
+	else if( !isPositiveNumber(amount) ) {
+		return false
+	}
+	else if( source.balance < amount ) {
+		return false
+	}
+	target.balance += amount
+	source.balance -= amount
+	return true
+}
+
+
+
+// A number >= 0
+function isPositiveNumber(maybe) {
+	if( (typeof maybe) !== 'number' ) {
+		return false
+	}
+	else if( maybe < 0 || maybe === Infinity || isNaN(maybe) ) {
+		return false
+	}
+	return true
 }
 
 function isAccount(maybeAccount) {
@@ -13,14 +59,11 @@ function isAccount(maybeAccount) {
 	else if( !maybeAccount.id ) {
 		return false
 	}
-	else if( (typeof maybeAccount.balance) !== 'number' ) {
-		return false
-	}
-	else if( maybeAccount.balance < 0 || maybeAccount.balance === Infinity || isNaN(maybeAccount.balance) ) {
+	else if( !isPositiveNumber(maybeAccount.balance) ) {
 		return false
 	}
 
 	return true
 }
 
-module.exports = { deposit /*, withdraw, transfer*/ }
+module.exports = { deposit, withdraw, transfer }
--- src/bank.test.js
+++ src/bank.test.js
@@ -1,21 +1,4 @@
-/*
-2.12 Skriv tester för, och implementera, följande funktioner som arbetar på Account-objekt. Ett Account beskriver ett bankkonto.
-
-interface Account {
-	id: string;
-	balance: number;
-}
-function deposit(account: Account, amount: number): void
-Ökar saldot på kontot med amount. Alla flyttal (floating point, normala JavaScript number) som rimligtvis kan tänkas motsvara ett pengabelopp är tillåtna värden. Om funktionen får ett otillåtet tal som parameter ska den kasta ett Error med ett lämpligt felmeddelande.
-
-
-function withdraw(account: Account, amount: number): void
-Minskar saldot på kontot med amount, förutsatt att det finns tillräckligt med pengar på kontot. Om det inte gör det ska funktionen inte dra några pengar utan i stället kasta ett Error med ett lämpligt felmeddelande. Samma sak om amount är ett otillåtet tal.
-
-function transfer(source: Account, target: Account, amount: number): boolean
-Genomför en transaktion: Minskar saldot på kontot med amount och ökar med motsvarande belopp på mottagarkontot, förutsatt att inget har gått fel. Om transaktionen misslyckas ska funktionen returnera false. Tips: det kan bli fel av flera anledningar.
-*/
-const { deposit /*, withdraw, transfer*/ } = require('./bank')
+const { deposit, withdraw, transfer } = require('./bank')
 
 describe('Bank functions', () => {
 	
@@ -43,7 +26,126 @@ describe('Bank functions', () => {
 			})
 		})
 
-		// it throws an Exception for invalid amount - not a number, negative value, Infinity, NaN
-	
+		it('throws an Exception for invalid amount', () => {
+			testAccount = { id: '987', balance: 240 }
+			testAmounts = ['anka', -1, Infinity, NaN]
+			// not a number, negative value, Infinity, NaN
+
+			testAmounts.forEach(amount => {
+				expect( () => deposit(testAccount, amount) ).toThrow()
+			})
+		})
+	})
+
+	describe('withdraw', () => {
+		it('decreases the balance with amount', () => {
+			const testAccount = { id: '5353', balance: 95 }
+			const testAmount = 25
+			const expectedBalance = testAccount.balance - testAmount
+
+			withdraw(testAccount, testAmount)
+
+			expect(testAccount.balance).toBe(expectedBalance)
+		})
+
+		it('throws an Exception for invalid Account object', () => {
+			const testAccounts = [null, {}, {id: '12345'}, {balance: 2020}]
+			const testAmount = 101
+
+			testAccounts.forEach(account => {
+				expect( () => withdraw(account, testAmount) ).toThrow()
+			})
+		})
+
+		it('throws an Exception for invalid amount', () => {
+			const testAccount = { id: '987', balance: 240 }
+			const testAmounts = ['anka', -1, Infinity, NaN]
+
+			testAmounts.forEach(amount => {
+				expect( () => withdraw(testAccount, amount) ).toThrow()
+			})
+		})
+
+		it('throws an Exception if amount > balance', () => {
+			const testAccount = { id: '404', balance: 1250 }
+			const testAmount = 1251
+
+			expect( () => withdraw(testAccount, testAmount) ).toThrow()
+		})
+	})
+
+	describe('transfer', () => {
+		it('transfers money from source to target', () => {
+			const testSource = { id: '456', balance: 256 }
+			const testTarget = { id: '457', balance: 300 }
+			const amount = 56
+			const expectedSourceBalance = testSource.balance - amount
+			const expectedTargetBalance = testTarget.balance + amount
+			
+			const actual = transfer(testSource, testTarget, amount)
+
+			expect(actual).toBe(true)
+			expect(testSource.balance).toBe(expectedSourceBalance)
+			expect(testTarget.balance).toBe(expectedTargetBalance)
+		})
+
+		it('returns false for invalid source Account', () => {
+			const testSource = 'bacon'
+			const testTarget = { id: '932', balance: 2322 }
+			const amount = 120
+
+			const actual = transfer(testSource, testTarget, amount)
+			expect(actual).toBe(false)
+		})
+
+		it('returns false for invalid target Account', () => {
+			const testSource = { id: '9300', balance: 232 }
+			const testTarget = 1001
+			const amount = 110
+
+			const actual = transfer(testSource, testTarget, amount)
+			expect(actual).toBe(false)
+		})
+
+		it('returns false for invalid amount', () => {
+			const testSource = { id: '6653432', balance: 333 }
+			const testTarget = { id: '34452', balance: 555 }
+
+			const testAmounts = ['anka', -1, Infinity, NaN]
+
+			testAmounts.forEach(amount => {
+				const actual = transfer(testSource, testTarget, amount)
+				expect(actual).toBe(false)
+			})
+		})
+
+		it('returns false if amount > source.balance', () => {
+			const testSource = { id: '23232', balance: 100 }
+			const testTarget = { id: '8575', balance: 5000 }
+			const amount = 101
+
+			const actual = transfer(testSource, testTarget, amount)
+			expect(actual).toBe(false)
+		})
 	})
 })
+
+
+/*
+2.12 Skriv tester för, och implementera, följande funktioner som arbetar på Account-objekt. Ett Account beskriver ett bankkonto.
+
+interface Account {
+	id: string;
+	balance: number;
+}
+function deposit(account: Account, amount: number): void
+Ökar saldot på kontot med amount. Alla flyttal (floating point, normala JavaScript number) som rimligtvis kan tänkas motsvara ett pengabelopp är tillåtna värden. Om funktionen får ett otillåtet tal som parameter ska den kasta ett Error med ett lämpligt felmeddelande.
+
+
+function withdraw(account: Account, amount: number): void
+Minskar saldot på kontot med amount, förutsatt att det finns tillräckligt med pengar på kontot. Om det inte gör det ska funktionen inte dra några pengar utan i stället kasta ett Error med ett lämpligt felmeddelande. Samma sak om amount är ett otillåtet tal.
+
+
+function transfer(source: Account, target: Account, amount: number): boolean
+Genomför en transaktion: Minskar saldot på kontot med amount och ökar med motsvarande belopp på mottagarkontot, förutsatt att inget har gått fel. Om transaktionen misslyckas ska funktionen returnera false. Tips: det kan bli fel av flera anledningar.
+*/
